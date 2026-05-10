e538c9c6e68b7f60f6d2d8271b8deb680c53ee0d
Felix Nardmann <felix.nardmann@gmx.de>
2026-05-10 22:03:39 +0200

    Fix headings of general recommendations

--- render-nutrition.js
+++ render-nutrition.js
@@ -679,14 +679,6 @@ const MIN_COVERAGE = 0.02;
       if (hiddenCount > 0) {
         plantHtml += `<button class="btn-secondary sugg-show-more" onclick="_expandSugg('plant')">${esc(t('sugg_show_more', { n: hiddenCount }))}</button>`;
       }
-
-      const suggLabel = _expandedNutrientKey ? t('sugg_vit_specific', { vit: esc(t('nutrient_' + _expandedNutrientKey)) }) : '';
-      plantHtml = suggLabel ? `<div class="sugg-section">
-        <div class="sugg-section-header">
-          <span class="sugg-section-nutrient">${suggLabel}</span>
-        </div>
-        <div class="sugg-food-list">${plantHtml}</div>
-      </div>` : plantHtml;
     }
   }
 
@@ -800,7 +792,7 @@ const MIN_COVERAGE = 0.02;
       </div>`;
   }
 
-  const generalHtml = (!_expandedNutrientKey && (generalPlantHtml || generalAnimalHtml))
+  const generalHtml = (generalPlantHtml || generalAnimalHtml)
     ? `<div class="sugg-section sugg-section--general">${generalPlantHtml}${generalAnimalHtml}</div>`
     : '';
 
